5c5f25ce3bb13e85b059e7ded70e4789a274b26c
Seba G <sebag.git@gmail.com>
2023-06-24 16:15:56 +0200

    synchronizer - clone github repo

--- synchronizer.sh
+++ synchronizer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #
+# GLOBALS
 # Read configuration
+GITHUB_TEMP_DIR="gh_temp"
+GITLAB_TEMP_DIR="gl_temp"
+REPO_TEMP_DIR="repo"
+
 CONFIG_DATA=$(cat config.json)
 GH_CONFIG_DATA=$(echo $CONFIG_DATA | jq .gh)
 declare -A GITHUB_CONFIG=(
@@ -16,9 +21,42 @@ declare -A GITLAB_CONFIG=(
 	[token]=$(echo $GL_CONFIG_DATA | jq -r .token)
 )
 
+# FUNCTIONS
+
+clone_github () {
+	local USERNAME=${GITHUB_CONFIG[username]}
+	local TOKEN=${GITHUB_CONFIG[token]}
+	local PROJECT=$1
+	local GIT_URL="https://$USERNAME:$TOKEN@github.com/$PROJECT.git"
+	git clone --bare $GIT_URL $REPO_TEMP_DIR/$GITHUB_TEMP_DIR
+}
+
+clone_repo () {
+	local PROVIDER=$(echo $1 | cut -d ':' -f1)
+	local REPO_PROJECT=$(echo $1 | cut -d ':' -f2)
+
+	echo "p: $PROVIDER rp: $REPO_PROJECT"
+
+	case $PROVIDER in
+		"gh"*)
+			clone_github $REPO_PROJECT
+			;;
+		"gl"*)
+			echo "UNIMPLEMENTED"
+			;;
+	esac
+}
+
+# MAIN
 # Read file with repos - exlclude empty line and comments
+mkdir -p $REPO_TEMP_DIR/$GITHUB_TEMP_DIR
+mkdir -p $REPO_TEMP_DIR/$GITLAB_TEMP_DIR
+
 REPOS=$(cat repos | grep -v '^$\|^#')
 
 for REPO in $REPOS; do
-	echo "r: $REPO"
+	ORIGIN_REPO_PROJECT=$(echo $REPO | cut -d '|' -f1)
+	BACKUP_REPO_PROJECT=$(echo $REPO | cut -d '|' -f2)
+	echo -e "o:\n $(clone_repo $ORIGIN_REPO_PROJECT)\nb:\n $(clone_repo $BACKUP_REPO_PROJECT) \n\n"
 done
+
